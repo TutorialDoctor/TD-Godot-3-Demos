330f9212e4a7d2120e3bbdd382a655137ca30bd0
Tutorial Doctor <thetutorialdoctor@gmail.com>
2017-12-28 10:38:22 -0500

    edits
    edits

---
+++ Godot 3 Demos/3D Test/Scripts/Rotate.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_D):
+		$Lady.rotate($translation,.1)
