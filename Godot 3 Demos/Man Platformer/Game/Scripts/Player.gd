fbd5009c2d41d273767f5c8b9439d337f0db9aef
Tutorial Doctor <thetutorialdoctor@gmail.com>
2018-03-22 10:05:30 -0400

    Jump Behavior Improved
    Can’t run in the air.

--- Godot 3 Demos/Man Platformer/Game/Scripts/Player.gd
+++ Godot 3 Demos/Man Platformer/Game/Scripts/Player.gd
@@ -6,7 +6,7 @@ var sprite
 var snd_jump
 var bounds
 
-var speed = 50
+var speed = 600
 var height = 400
 
 func _ready():
@@ -16,8 +16,8 @@ func _ready():
 	# Also you have to make sure all collisions are reported
 	#player.set_max_contacts_reported(true)
 	player.set_contact_monitor(true)
-	player.set_max_contacts_reported(1) 
-	
+	player.set_max_contacts_reported(1)
+
 	sprite = $Sprite
 	feet = $Feet
 	feet.enabled = true
@@ -35,12 +35,14 @@ func _input(event):
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		sprite.flip_h = false
-		sprite.animation = "run"
-		player.apply_impulse(Vector2(0,0),Vector2(speed,0))
+		if feet.is_colliding():
+			sprite.animation = "run"
+		player.set_axis_velocity(Vector2(speed,0))
 	elif Input.is_action_pressed("ui_left"):
 		sprite.flip_h = true
-		sprite.animation = "run"
-		player.apply_impulse(Vector2(0,0),Vector2(-speed,0))
+		if feet.is_colliding():
+			sprite.animation = "run"
+		player.set_axis_velocity(Vector2(-speed,0))
 	elif not feet.is_colliding():
 		sprite.animation = "air"
 	elif feet.is_colliding():
@@ -51,6 +53,6 @@ func _physics_process(delta):
 			player.apply_impulse(Vector2(0,0),Vector2(0,-height))
 			if Input.is_action_just_pressed("ui_up"):
 				snd_jump.playing= true
-	
+
 	if bounds in player.get_colliding_bodies():
 		get_tree().reload_current_scene()
